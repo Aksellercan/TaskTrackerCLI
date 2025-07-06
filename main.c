dcd1a4b20cb03ff2860aa61edabb2e67db40cce6
Aksell <akselercan2003@gmail.com>
2025-07-06 22:12:45 +0200

    refactor

--- main.c
+++ main.c
@@ -40,43 +40,29 @@ int main(int argc, char *argv[]) {
         if (argc == 3) {
             if (compareStrings(argv[1], "add")) {
                 addOption(argv[2]);
-                free_tasks(); //free memory
-                return 0;
             }
             if (compareStrings(argv[1], "delete")) {
                 int intValue =atoi(argv[2]);
                 remove_task(intValue);
-                free_tasks(); //free memory
-                return 0;
             }
             if (compareStrings(argv[1], "list")) {
                 if (compareStrings(argv[2], "todo")) {
                     sort_task_by_todo();
-                    free_tasks(); //free memory
-                    return 0;
                 }
                 if (compareStrings(argv[2], "in-progress")) {
                     sort_task_by_in_progress();
-                    free_tasks(); //free memory
-                    return 0;
                 }
                 if (compareStrings(argv[2], "done")) {
                     sort_task_by_completed();
-                    free_tasks(); //free memory
-                    return 0;
                 }
             }
             if (compareStrings(argv[1], "mark-in-progress")) {
                 int intValue =atoi(argv[2]);
                 mark_task_status(intValue, "in-progress");
-                free_tasks(); //free memory
-                return 0;
             }
             if (compareStrings(argv[1], "mark-done")) {
                 int intValue =atoi(argv[2]);
                 mark_task_status(intValue, "done");
-                free_tasks(); //free memory
-                return 0;
             }
         }
 
@@ -84,27 +70,21 @@ int main(int argc, char *argv[]) {
             if (compareStrings(argv[1], "update")) {
                 int intValue =atoi(argv[2]);
                 update_task(intValue, argv[3]);
-                free_tasks(); //free memory
-                return 0;
             }
         }
 
         if (argc == 2) {
             if (compareStrings(argv[1], "list")) {
                 list_tasks();
-                free_tasks(); //free memory
-                return 0;
             }
 
             if (compareStrings(argv[1], "help")) {
                 help();
-                return 0;
             }
         }
-        free_tasks(); //free memory
+        free_tasks();
     } else {
         help();
-        return 0;
     }
-    return 1;
+    return 0;
 }
